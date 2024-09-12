22e611b4318630fa24a90d062de1752711e254e5
Arnaldo Becker <a.becker@hoteismabu.com.br>
2024-09-12 09:38:02 -0300

    alter .env frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL='http://api-test-joinads.test/api/tasks'
+const API_BASE_URL=process.env.REACT_APP_API_URL;
+
 
 const api = axios.create({
     baseURL: API_BASE_URL,
