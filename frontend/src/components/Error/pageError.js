0cf864f4dbb83358a48aa64286453f15c4a26d1f
Arnaldo Becker <a.becker@hoteismabu.com.br>
2024-09-12 09:38:32 -0300

    add new page Error

--- frontend/src/components/Error/pageError.js
+++ frontend/src/components/Error/pageError.js
@@ -1,9 +1,18 @@
-const ErrorPage = ({message}) =>{
-    return (
-        <div>
-           {message}
-        </div>
-    )
-}
+import React from 'react';
+import styles from './pageError.module.css';
 
-export default ErrorPage
+const ErrorPage = ({ message }) => {
+    const handleReload = () => {
+        window.location.reload();
+    };
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.heading}>Oops!</h1>
+            <p className={styles.message}>{message || "Algo deu errado. Por favor, tente novamente mais tarde."}</p>
+            <button className={styles.button} onClick={handleReload}>Recarregar</button>
+        </div>
+    );
+};
+
+export default ErrorPage;
